01ca07e6a5455e9a43e2e62422deffd2a66c977c
Eduardo Jose Evangelista <202215020025@ad.ifpb.edu.br>
2025-02-13 20:24:45 -0300

    first endpoint

---
+++ src/main/java/testeunitario/controllers/UserController.java
@@ -0,0 +1,14 @@
+package testeunitario.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    @GetMapping
+    public String getUser(){
+        return "get user was called muito massa doido";
+    }
+}
