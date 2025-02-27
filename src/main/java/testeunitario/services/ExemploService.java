fafd41d47213053890b466ab7f969c58fbc053c7
Eduardo Jose Evangelista <202215020025@ad.ifpb.edu.br>
2025-02-27 20:47:44 -0300

    JUnit att

--- src/main/java/testeunitario/services/ExemploService.java
+++ src/main/java/testeunitario/services/ExemploService.java
@@ -1,5 +1,7 @@
 package testeunitario.services;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,6 +9,33 @@ public class ExemploService {
 
     public int somaValores(int x, int y){
         return x + y;
+    }
+
+    public String verificarNulo(String objeto){
+        return objeto;
+    }
+
+    public boolean verificarSeMaiorQue(double valor1, double valor2){
+        return valor1 > valor2;
+    }
+
+    public String[] retornarQuatroPrimeirasLetrasAlfabeto(){
+
+        String[] alfabeto = {"A", "B", "C", "D"};
+
+        return alfabeto;
+    }
+
+    public ArrayList<String> retornarCincoPrimeirasLetrasAlfabeto(){
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+
+        return list;
     }
 
 }
--- src/test/java/testeunitario/services/ExemploServiceTest.java
+++ src/test/java/testeunitario/services/ExemploServiceTest.java
@@ -1,23 +1,82 @@
 package testeunitario.services;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.MethodOrderer;
+//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
-
+@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // Em geral, os testes não precisam de ordem, porém existem situações em que ordenar o projeto é util
 public class ExemploServiceTest {
 
     @Test
+    @DisplayName("Testae somar valor e verificar se são ou não iguais")
+    //assert Equals
     void testeSomarValorVerificarSeIgualENaoIgual(){
 
-        //configurar
+        // //configurar
+        // ExemploService exemploService = new ExemploService();
+        // int resultadoEsperado = 6;
+
+        // //executar
+        // int resultadoReal = exemploService.somaValores(2, 4);
+
+        // // verificar (assert)
+        // Assertions.assertEquals(resultadoEsperado, resultadoReal, "2 + 4 deve ser 6");
+
         ExemploService exemploService = new ExemploService();
-        int resultadoEsperado = 6;
 
-        //executar
-        int resultadoReal = exemploService.somaValores(2, 4);
-
-        // verificar (assert)
-        Assertions.assertEquals(resultadoEsperado, resultadoReal, "2 + 4 deve ser 6");
+        assertEquals(6, exemploService.somaValores(2, 4), "2 + 4 deve ser igual a 6");
     }
 
+    //assert Null
+    @Test
+    void testeVerificarSeObjetoNulo(){
+        ExemploService exemploService = new ExemploService();
+        assertNull(exemploService.verificarNulo(null), "objeto deve ser nulo");
+        assertNotNull(exemploService.verificarNulo("string"), "objeto não deve ser nulo");
+    }
+
+    // assert true ou false
+    @Test
+    void testeVerificarSeValorMaiorQue(){
+        ExemploService exemploService = new ExemploService();
+        assertTrue(exemploService.verificarSeMaiorQue(10, 5), "valor 1 deve ser maior que valor 2");
+        assertFalse(exemploService.verificarSeMaiorQue(5, 50), "valor 1 deve ser menor que valor 2");
+    }
+
+    //assert para arrays
+    @Test
+    void testeVerificarSeArraySaoIguais(){
+        System.out.println("Metodo: testeVerificarSeArraySaoIguais");
+        ExemploService exemploService = new ExemploService();
+        String[] expectedArray = {"A", "B", "C", "D"};
+
+        assertArrayEquals(expectedArray, exemploService.retornarQuatroPrimeirasLetrasAlfabeto());
+    }
+
+    // assert iterable
+    @Test
+    void testeVerificarSeArrayListsSaoIguais(){
+        System.out.println("Método: testeVerificarSeArrayListsSaoIguais");
+
+        ExemploService exemploService = new ExemploService();
+        ArrayList <String> expectedArray = new ArrayList <String> ();
+        
+        expectedArray.add("A");
+        expectedArray.add("B");
+        expectedArray.add("C");
+        expectedArray.add("D");
+        expectedArray.add("E");
+
+        assertIterableEquals(expectedArray, exemploService.retornarCincoPrimeirasLetrasAlfabeto());
+
+    }
 }
